5fd7374e9cd3653c0f48493187c83a5227dbb9a7
Fadli Rizaldy <fadli.rizaldy@@shopee.com>
2023-12-21 21:58:46 +0700

    fix:payload fetcher

--- src/services/fetcher/fetcher.ts
+++ src/services/fetcher/fetcher.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 
 export const fetcherGet = (url: string) => axios.get(url).then((res) => res.data);
 
-export const fetcherPost = (url: string, payload) => axios.post(url, payload).then((res) => res.data);
+export const fetcherPost = (url: string, payload: any) => axios.post(url, payload).then((res) => res.data);
 
-export const fetcherPut = (url: string, payload) => axios.put(url, payload).then((res) => res.data);
+export const fetcherPut = (url: string, payload: any) => axios.put(url, payload).then((res) => res.data);
 
-export const fetcherPatch = (url: string, payload) => axios.patch(url, payload).then((res) => res.data);
+export const fetcherPatch = (url: string, payload: any) => axios.patch(url, payload).then((res) => res.data);
 
 export const fetcherDelete = (url: string) => axios.delete(url).then((res) => res.data);
